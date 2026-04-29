3d1624b528ac6e3d5bab21b07a3793417ce29399
aishtugan <artur.ishtuganov@gmail.com>
2026-04-29 01:02:55 -0300

    Complete banking system project

--- Simple Banking System (Java)/task/src/banking/Main.java
+++ Simple Banking System (Java)/task/src/banking/Main.java
@@ -15,6 +15,9 @@ public class Main {
 
         String command1 = "1. Create an account";
         String command2 = "2. Log into account";
+        String command3 = "";
+        String command4 = "";
+        String command5 = "";
         String command0 = "0. Exit";
 
         Account currentAccount = null;
@@ -22,12 +25,10 @@ public class Main {
         boolean continueLoop = true;
 
         String dbFileName = null;
-        if (args.length > 0) {
-            for (int i = 0; i < args.length - 1; i++) {
-                if ("-fileName".equals(args[i])) {
-                    dbFileName = args[i + 1];
-                    break;
-                }
+        for (int i = 0; i < args.length - 1; i++) {
+            if ("-fileName".equals(args[i])) {
+                dbFileName = args[i + 1];
+                break;
             }
         }
 
@@ -39,6 +40,15 @@ public class Main {
 
             System.out.println(command1);
             System.out.println(command2);
+            if (!command3.isEmpty()) {
+                System.out.println(command3);
+            }
+            if (!command4.isEmpty()) {
+                System.out.println(command4);
+            }
+            if (!command5.isEmpty()) {
+                System.out.println(command5);
+            }
             System.out.println(command0);
 
             int choice = sc.nextInt();
@@ -90,19 +100,87 @@ public class Main {
 
                             currentAccount = foundAccount;
                             command1 = "1. Balance";
-                            command2 = "2. Log out";
+                            command2 = "2. Add income";
+                            command3 = "3. Do transfer";
+                            command4 = "4. Close account";
+                            command5 = "5. Log out";
+                            command0 = "0. Exit";
 
                             System.out.println("You have successfully logged in!\n");
                             break;
 
-                        case "2. Log out":
+                        case "2. Add income":
 
-                            currentAccount = null;
-                            command1 = "1. Create an account";
-                            command2 = "2. Log into account";
-                            System.out.println("You have successfully logged out!\n");
+                            System.out.println("Enter income:");
+                            Integer inputIncome = sc.nextInt();
+                            currentAccount.deposit(inputIncome);
+                            bankService.updateAccountToDB(con, currentAccount);
+                            System.out.println("Income was added!\\n");
                             break;
                     }
+                    break;
+
+                case 3: //do transfer
+
+                    System.out.println("Transfer");
+                    System.out.println("Enter card number:");
+                    String inputCardNumber = sc.next();
+                    if (inputCardNumber.length() == 16) {
+                        String lastNumber = inputCardNumber.substring(15, 16);
+                        int checksum = bankService.getCardControlSumByLuhn(inputCardNumber.substring(0, 15));
+                        StringBuilder sb = new StringBuilder();
+                        if(!sb.append(checksum).toString().equals(lastNumber)) {
+                            System.out.println("Probably you made a mistake in the card number. Please try again!");
+                            break;
+                        }
+                    }
+
+                    Account foundAccount = bankService.findAccount(con, inputCardNumber);
+                    if (foundAccount == null) {
+                        System.out.println("Such a card does not exist.");
+                        break;
+                    }
+
+                    System.out.println("Enter how much money you want to transfer:");
+                    Integer transferAmount = sc.nextInt();
+
+                    if (!bankService.makeTransfer(currentAccount, foundAccount, transferAmount)){
+                        break;
+                    }
+
+                    bankService.updateAccountToDB(con, currentAccount);
+                    bankService.updateAccountToDB(con, foundAccount);
+                    System.out.println("Success!\n");
+
+                    break;
+
+                case 4: //close account
+
+                    bankService.accounts.remove(currentAccount);
+                    bankService.deleteAccountFromDB(con, currentAccount);
+                    System.out.println("The account has been closed!\n");
+
+                    currentAccount = null;
+                    command1 = "1. Create an account";
+                    command2 = "2. Log into account";
+                    command3 = "";
+                    command4 = "";
+                    command5 = "";
+                    command0 = "0. Exit";
+
+                    break;
+
+                case 5: //log out
+
+                    currentAccount = null;
+                    command1 = "1. Create an account";
+                    command2 = "2. Log into account";
+                    command3 = "";
+                    command4 = "";
+                    command5 = "";
+                    command0 = "0. Exit";
+
+                    System.out.println("You have successfully logged out!\n");
                     break;
 
                 case 0:
@@ -121,7 +199,7 @@ public class Main {
 }
 
 class DatabaseManager {
-    private static final String dbPath = "Simple Banking System (Java)/task/db/simplebanksystem.s3db";
+    private static final String dbPath = "card.s3db";
     private static final String dbUrl = "jdbc:sqlite:";
 
     public static Connection getConnection(String dbFileName) throws SQLException {
@@ -144,10 +222,10 @@ class DatabaseManager {
         }
     }
 
-    public static void createTable(Connection conn, String tableName, Map columns) {
+    public static void createTable(Connection conn, String tableName, Map<String, String> columns) {
 
         String sqlCommand = "CREATE TABLE IF NOT EXISTS " + tableName + " (";
-        for (Object key : columns.keySet()) {
+        for (String key : columns.keySet()) {
             sqlCommand += "     " + key + " " + columns.get(key) + ",";
         }
         sqlCommand = sqlCommand.substring(0, sqlCommand.length() - 1).concat(");");
@@ -180,20 +258,76 @@ class DatabaseManager {
         }
     }
 
-    public static List<Account> findAccountsByParameters(Connection conn, String table, Map parameters) throws SQLException {
-        List<Account> accounts = new ArrayList<>();
-        List<Object>  values = new ArrayList<>();
-        String sql = "SELECT * FROM " + table + " WHERE ";
-        int i = 1;
+    public static void updateRow(Connection conn, String table, Map<String, Object> parameters, Map<String, Object> values) throws SQLException {
+
+        String sqlCommand = "UPDATE " + table + " SET ";
+        for (Object key : values.keySet()) {
+            sqlCommand += " " + key + " = ? ,";
+        }
+
+        sqlCommand = sqlCommand.substring(0, sqlCommand.length() - 1).concat("\n").concat("WHERE ") ;
+
         for (Object key : parameters.keySet()) {
-            sql += key + " = " + parameters.get(key);
+            sqlCommand += " " + key + " = ? AND";
+        }
+
+        sqlCommand = sqlCommand.substring(0, sqlCommand.length() - 4);
+
+        try (PreparedStatement stmt = conn.prepareStatement(sqlCommand)) {
+            int i = 1;
+
+            for (String key : values.keySet()) {
+                stmt.setObject(i, values.get(key));
+                i++;
+            }
+
+            for (String key : parameters.keySet()) {
+                stmt.setObject(i, parameters.get(key));
+                i++;
+            }
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteRow(Connection conn, String table, Map<String, Object> parameters) throws SQLException {
+
+        String sqlCommand = "DELETE FROM " + table + " WHERE ";
+        for (Object key : parameters.keySet()) {
+            sqlCommand += " " + key + " = ? ,";
+        }
+
+        sqlCommand = sqlCommand.substring(0, sqlCommand.length() - 2);
+
+        try (PreparedStatement stmt = conn.prepareStatement(sqlCommand)) {
+            int i = 1;
+
+            for (String key : parameters.keySet()) {
+                stmt.setObject(i, parameters.get(key));
+                i++;
+            }
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Account> findAccountsByParameters(Connection conn, String table, Map<String, Object> parameters) throws SQLException {
+
+        List<Account> accounts = new ArrayList<>();
+
+        String sqlString = "SELECT * FROM " + table + " WHERE ";
+        int i = 1;
+        for (String key : parameters.keySet()) {
+            sqlString += key + " = " + parameters.get(key);
             i++;
             if (i < parameters.size()) {
-                sql.concat(" AND ");
+                sqlString.concat(" AND ");
             }
         }
 
-        try (PreparedStatement stmt = conn.prepareStatement(sql);
+        try (PreparedStatement stmt = conn.prepareStatement(sqlString);
 
             ResultSet rs = stmt.executeQuery()) {
 
@@ -240,11 +374,11 @@ class Account {
     }
 
     void deposit(Integer amount) {
-        balance += amount;
+        this.balance += amount;
     }
 
     void withdraw(Integer amount) {
-        balance -= amount;
+        this.balance -= amount;
     }
 
     void generateCardNumber(BankingSystem bankingSystem, String bankBIN, int lengthOfNumber) {
@@ -262,8 +396,7 @@ class Account {
             sb.append(digit);
         }
 
-//       int checksum = rand.nextInt(10); //for the first step
-        int checksum = getCardControlSumByLuhn(sb.toString());
+        int checksum = BankService.getCardControlSumByLuhn(sb.toString());
         this.cardNumber = sb.append(checksum).toString();
     }
 
@@ -279,6 +412,111 @@ class Account {
             sb.append(digit);
         }
         this.pin = sb.toString();
+    }
+
+}
+
+class BankService {
+
+    public final String bankBIN = "400000";
+    public final int cardNumberOfDigits = 16;
+    public final int pinNumberOfDigits = 4;
+    public final String accountDBName = "card";
+
+    List<Account> accounts = new ArrayList<>();
+    List<BankingSystem> bankingSystems = new ArrayList<>();
+
+    Account findAccount(Connection conn, String cardNumber) throws SQLException {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("number", cardNumber);
+
+        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
+        for (Account account : foundAccounts) {
+            return account;
+        }
+        return null;
+    }
+
+    void saveAccountToDB(Connection conn, Account account) throws SQLException {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("number", account.cardNumber);
+
+        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
+
+        if (!foundAccounts.isEmpty()) {return;}
+
+        DatabaseManager.insertRow(conn, accountDBName, List.of("number", "pin", "balance"), List.of(account.cardNumber, account.pin, account.balance));
+
+    }
+
+    void deleteAccountFromDB(Connection conn, Account account) throws SQLException {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("number", account.cardNumber);
+
+        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
+
+        if (foundAccounts.isEmpty()) {return;}
+
+        DatabaseManager.deleteRow(conn, accountDBName, parameters);
+
+    }
+
+    void updateAccountToDB(Connection conn, Account account) throws SQLException {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("number", account.cardNumber);
+
+        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
+
+        if (foundAccounts.isEmpty()) {return;}
+
+        Map<String, Object> values = new HashMap<>();
+
+        values.put("number",  account.cardNumber);
+        values.put("balance", account.balance);
+        values.put("pin",     account.pin);
+
+        DatabaseManager.updateRow(conn, accountDBName, parameters, values);
+
+    }
+
+    void createAccountTable(Connection conn) throws SQLException {
+
+        Map<String, String> columns = new HashMap<>();
+
+        columns.put("id", "INTEGER PRIMARY KEY");
+        columns.put("number", "TEXT UNIQUE NOT NULL");
+        columns.put("pin", "TEXT NOT NULL");
+        columns.put("balance", "INTEGER NOT NULL DEFAULT 0");
+
+        DatabaseManager.createTable(conn, accountDBName, columns);
+    }
+
+    boolean makeTransfer(Account accountOut, Account  accountIn, Integer amount) {
+        if (null == accountIn) {
+            return false;
+        }
+        if (null == accountOut) {
+            return false;
+        }
+        if (Objects.equals(accountOut, accountIn)) {
+            System.out.println("You can't transfer money to the same account!");
+            return false;
+        }
+        if (accountOut.balance < amount) {
+            System.out.println("Not enough money!");
+            return false;
+        }
+        accountOut.withdraw(amount);
+        accountIn.deposit(amount);
+        return true;
+    }
+
+    boolean pinIsCorrect(Account account, String pin) {
+        return account.pin.equals(pin);
     }
 
     static int getCardControlSumByLuhn(String cardCutNumber) {
@@ -303,56 +541,5 @@ class Account {
 
         return sumOfDigits % 10 == 0 ? 0 : 10 - sumOfDigits % 10;
     }
-}
 
-class BankService {
-
-    public final String bankBIN = "400000";
-    public final int cardNumberOfDigits = 16;
-    public final int pinNumberOfDigits = 4;
-    public final String accountDBName = "card";
-
-    List<Account> accounts = new ArrayList<>();
-    List<BankingSystem> bankingSystems = new ArrayList<>();
-
-    Account findAccount(Connection conn, String cardNumber) throws SQLException {
-
-        Map parameters = new HashMap();
-        parameters.put("number", cardNumber);
-
-        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
-        for (Account account : foundAccounts) {
-            return account;
-        }
-        return null;
-    }
-
-    void saveAccountToDB(Connection conn, Account account) throws SQLException {
-
-        Map parameters = new HashMap();
-        parameters.put("number", account.cardNumber);
-
-        List<Account> foundAccounts = DatabaseManager.findAccountsByParameters(conn, accountDBName, parameters);
-
-        if (!foundAccounts.isEmpty()) {return;}
-
-        DatabaseManager.insertRow(conn, accountDBName, List.of("number", "pin", "balance"), List.of(account.cardNumber, account.pin, account.balance));
-
-    }
-
-    void createAccountTable(Connection conn) throws SQLException {
-
-        Map columns = new HashMap();
-
-        columns.put("id", "INTEGER PRIMARY KEY");
-        columns.put("number", "TEXT UNIQUE NOT NULL");
-        columns.put("pin", "TEXT NOT NULL");
-        columns.put("balance", "INTEGER NOT NULL DEFAULT 0");
-
-        DatabaseManager.createTable(conn, accountDBName, columns);
-    }
-
-    boolean pinIsCorrect(Account account, String pin) {
-        return account.pin.equals(pin);
-    }
 }
